2a13f8d03cb372ce6ce5db7396afebbb0a938f9f
William <willtf.wb@gmail.com>
2025-12-21 18:36:25 -0500

    :sparkles: skeleton ajust  Cambios a ser confirmados: 	modificados:     frontend/src/components/TransactionListSkeleton.jsx

--- frontend/src/components/TransactionListSkeleton.jsx
+++ frontend/src/components/TransactionListSkeleton.jsx
@@ -7,7 +7,7 @@ export default function TransactionListSkeleton() {
         <div key={i} className='flex justify-between items-center py-3 border-b last:border-0'>
           <div className='space-y-2'>
             <div className='h-4 w-32 bg-gray-200 rounded' />
-            <div className='h-3 w-24 bg-gray-100 rounded' />
+            <div className='h-3 w-24 bg-gray-100 rounded md:hidden' />
           </div>
 
           <div className='h-4 w-20 bg-gray-200 rounded' />
